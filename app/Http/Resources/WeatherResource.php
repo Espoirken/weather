6dc8c4ad2bef616cbbd546fb3cb6562a17ab1bb3
Gilbert Caburnay <gcaburnay@i4asiacorp.com>
2022-10-09 16:02:48 +0800

    update weather resource format

--- app/Http/Resources/WeatherResource.php
+++ app/Http/Resources/WeatherResource.php
@@ -24,8 +24,8 @@ class WeatherResource extends JsonResource
             'temp_min' => $this->main->temp_min . ' °C',
             'temp_max' => $this->main->temp_max . ' °C',
             'wind_speed' => $this->wind->speed . ' km/h',
-            'time' => Carbon::createFromTimestamp($this->dt)->format('h:i a'),
-            'day' => Carbon::createFromTimestamp($this->dt)->format('l'),
+            'time' => Carbon::createFromTimestamp($this->dt)->format('h:i A'),
+            'day' => Carbon::createFromTimestamp($this->dt)->format('D'),
             'description' => collect($this->weather)->map(fn($weather) => ucfirst($weather->description)),
             'icon' =>  collect($this->weather)->transform(fn($weather) => config('services.open_weather.icon_url') . '/' . $weather->icon. '.png'),
         ];
